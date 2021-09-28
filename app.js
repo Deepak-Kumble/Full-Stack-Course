f3b0a056ee1a1d5ef7ab260ff7ad19a65f3024e0
Deepak-Kumble <deepakkumble22@gmail.com>
2021-09-28 19:32:09 +0530

    operators

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-var arr=[1,2,3,4]
-arr.push(5)
-console.log(arr.length)
+var a=2
+var b=10
+
+console.log('a+b=',a+b)
+console.log('b-a=',b-a)
+console.log('b/a=',b/a)
+console.log('a*b=',a*b)
+console.log('a**b=',a**b)
+console.log('a%b=',a%b)
+
+
+
+var d=10
+var e=2
+console.log(d>e)
+console.log(d<e)
+console.log(d>=e)
+console.log(d<=e)
+console.log(d==e)
+console.log(d!=e)
